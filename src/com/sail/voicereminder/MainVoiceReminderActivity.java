a35373a634c9ec042108e763ea2f141f7a2efa76
“olivia0108” <min.yin@monstar-lab.com>
2014-11-04 15:55:19 +0800

    first commit ，creat the project and design a simple main activity layout。

---
+++ src/com/sail/voicereminder/MainVoiceReminderActivity.java
@@ -0,0 +1,19 @@
+package com.sail.voicereminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+
+public class MainVoiceReminderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main_voice_reminder);
+    }
+
+}

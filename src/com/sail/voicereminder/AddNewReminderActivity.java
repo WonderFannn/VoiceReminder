58cd7742a84445e689cd7c0e56b6bc71f38c4879
汪帆 <fan.wang@monstar-lab.com>
2015-03-27 16:32:48 +0800

    主界面的 view 获得....另增加添加界面

---
+++ src/com/sail/voicereminder/AddNewReminderActivity.java
@@ -0,0 +1,16 @@
+package com.sail.voicereminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddNewReminderActivity extends Activity{
+       @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_new_reminder);
+
+       
+       
+    }
+    
+}
--- src/com/sail/voicereminder/MainVoiceReminderActivity.java
+++ src/com/sail/voicereminder/MainVoiceReminderActivity.java
@@ -1,17 +1,54 @@
 package com.sail.voicereminder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainVoiceReminderActivity extends Activity {
+
+    private ImageView ivMenu;
+    private LinearLayout llClassifySelect;
+    private TextView tvClassify;
+    private ImageView ivSearch;
+    private ImageView ivAddNewReminder;
+    private ListView listView1;
+
+    private void findViews() {
+        ivMenu = (ImageView)findViewById( R.id.iv_menu );
+        llClassifySelect = (LinearLayout)findViewById( R.id.ll_classify_select );
+        tvClassify = (TextView)findViewById( R.id.tv_classify );
+        ivSearch = (ImageView)findViewById( R.id.iv_search );
+        ivAddNewReminder = (ImageView)findViewById( R.id.iv_add_new_reminder );
+        listView1 = (ListView)findViewById( R.id.listView1 );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_voice_reminder);
+        
+        findViews();
+       
+        ivAddNewReminder.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+
+                Intent addIntent = new Intent(this,AddNewReminderActivity.class);
+                startActivity(addIntent);
+                
+            }
+        });
+        
     }
 
 }
